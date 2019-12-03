3970ccd93b38a76175fe954c8835e442ce892a6a
sho Taguchi <s.taguchi0703@gmail.com>
2019-12-04 03:02:18 +0900

    アリはACできなかった。サンプルは通るがテストケースがNG

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## はじめに
 
+筆者はAtCoderを取り組み始めたアラフォー・ザコーダである。
+
+[D問題をコンテスト中にACしたことがないので、特訓が必要である](https://qiita.com/tagtagtag/items/f9ae5a1a0c24d04ae923)
+
 [AtCoder 版！蟻本 (初級編)](https://qiita.com/drken/items/e77685614f3c6bf86f44)に蟻本記載例題の類似問題が記載されている。
 
 [AtCoder](https://atcoder.jp/?lang=ja)を利用してジャッジできるアルゴリズムの良問が選別されているので、初学者にうってつけである。
@@ -17,7 +21,8 @@
 
 ## 今後
 
-* 上記記事に掲載された問題の完走を目指す。
+* 2-1 すべての基本 "全探索"に挑戦する。
+* 力をつけて蟻さんをやっつける。
 
 ## 解答
 
@@ -154,3 +159,112 @@ if is_search:
     print('-1 -1 -1')
 ```
 
+### 例題 1-6-2　Ants (POJ No.1852)
+* AtCoder
+  * [AGC 013 C Ants on a Circle](https://atcoder.jp/contests/agc013/tasks/agc013_c)
+
+* 方針
+  * むりすぎる。ギブ
+    * >(蟻のオマージュにして類似の発想をする問題です...が、ものすごく難しいのでここから下の問題たちをやって実力をつけてから挑むのがよさそうです、はむこさん提供)　
+    * 無謀にもトライしてみたが解説を読んでも実装の仕方が分からない。
+    * 素直に現象を再現させてみたがTLEした。$N=10^9$に$N=10^9$をネストさせることになるから、そりゃ無理だ。
+    * 解説を読み、ゼッケンを受け渡す様な方法を試してみた。
+    * サンプルコードは通ったが何でテストケースが通らないかが分からない。
+  
+* 実装
+  * ACできなかったNG品です
+  * 冗長な感じからして正解できなさそう。。。
+
+```python
+N, L, T = [int(item) for item in input().split()]
+pos_list = [[int(item) for item in input().split()] for _ in range(N)]
+
+first_ant = pos_list[0]
+# N 匹の蟻がいて、それぞれの蟻は 1 から N まで番号の書かれたゼッケンをつけている。
+# 二つの蟻が同じ場所についた瞬間、その二つの蟻はゼッケンを交換し、そのまますれ違って同じ方向に歩き続ける。
+# 上記問題の言い換えに生息するfirst ant さんの操作後の位置とゼッケンを求める
+if first_ant[1] == 1: #一番の子が1向きの場合
+
+    last_pos_of_first_ant = (first_ant[0] + T ) % L
+    num_of_reverse_ant = [item[1] for item in pos_list].count(2)
+
+    cnt = 0
+    reverse_ant_pos = [item[0] for item in pos_list if item[1] == 2]
+    cnt += T//L * len(reverse_ant_pos)
+
+    for i in reverse_ant_pos:
+        if (i - first_ant[0])//2 +1 <= (T % L):
+            cnt += 1
+    
+    last_num_of_first_ant = (1 + cnt) % N
+    if last_num_of_first_ant == 0:
+        last_num_of_first_ant += N
+
+else:
+    last_pos_of_first_ant = (first_ant[0] - T ) % L
+    num_of_reverse_ant = [item[1] for item in pos_list].count(1)
+
+    cnt = 0
+    reverse_ant_pos = [item[0] for item in pos_list if item[1] == 1]
+
+    cnt += T//L * len(reverse_ant_pos)
+
+    for i in reverse_ant_pos:
+        if ((first_ant[0] -(i-L))//2 + 1) <= (T % L):
+            cnt += 1
+            # print(T % L)
+            # print(i, first_ant[0] -(i-L),(first_ant[0] -(i-L))//2 + 1) 
+            # print('cnt', cnt)
+    
+    last_num_of_first_ant = (1 - cnt) % N
+    if last_num_of_first_ant <= 0:
+        last_num_of_first_ant += N
+
+# アリの位置を求める
+last_ant_pos = []
+
+for ant_pos, direction  in pos_list:
+    if direction == 1:
+        last_ant_pos.append((ant_pos + T)%L)
+    else:
+        ant_pos = (ant_pos - T)%L
+        if ant_pos < 0:
+            ant_pos += L
+        last_ant_pos.append(ant_pos)
+
+# first ant さんは位置Xにいるので、アリの位置リストからどれがfirst antさんか特定する。
+# 特定したらその位置にゼッケンを設定する。
+# 1方向のfirst antさんは、その他のゼッケンは時計回りに+1する（他のアリを1減らしながら回っているから）
+# 2方向のfirst antさんは、その他のゼッケンを時計回りに-1する（他のアリを1増やしながら回っているから。逆回りに・・・で考えたほうが分かりよいか？）
+# 2方向の場合はできたリストを逆順に出力する    
+res = []
+if first_ant[1] == 1:
+    for i, pos in enumerate(last_ant_pos):
+            temp_num = (last_num_of_first_ant+i)%N
+            if temp_num == 0:
+                temp_num = N
+            res.append([pos, temp_num])
+
+    res.sort(key=lambda x:x[1])
+else:
+    for i, pos in enumerate(last_ant_pos):
+        temp_num = (last_num_of_first_ant-i)%N
+        if temp_num == 0:
+            temp_num = N
+        res.append([pos, temp_num])
+
+    res.sort(key=lambda x:x[1], reverse=True)
+
+
+
+# print(first_ant)
+# print(num_of_reverse_ant)
+# print(reverse_ant_pos)
+# print('collison', cnt)
+# print('first ant of pos, num',last_pos_of_first_ant, last_num_of_first_ant)
+# print(last_ant_pos)
+# print(res)
+# print('-------ans--------')
+print(*[item[0] for item in res])
+
+```
--- ant_1-6-2_AGC013C/AGC013.py
+++ ant_1-6-2_AGC013C/AGC013.py
@@ -19,45 +19,90 @@ sys.stdin=f
 N, L, T = [int(item) for item in input().split()]
 pos_list = [[int(item) for item in input().split()] for _ in range(N)]
 
-num = 0
-next_list = []
+first_ant = pos_list[0]
+# N 匹の蟻がいて、それぞれの蟻は 1 から N まで番号の書かれたゼッケンをつけている。
+# 二つの蟻が同じ場所についた瞬間、その二つの蟻はゼッケンを交換し、そのまますれ違って同じ方向に歩き続ける。
+# 上記問題の言い換えに生息するfirst ant さんの操作後の位置とゼッケンを求める
+if first_ant[1] == 1: #一番の子が1向きの場合
 
-for i, item in enumerate(pos_list):
-    a, b = item
-    next_list.append([a, b, i])
+    last_pos_of_first_ant = (first_ant[0] + T ) % L
+    num_of_reverse_ant = [item[1] for item in pos_list].count(2)
 
-while T >= 1:
-    pos_list, next_list = next_list, []
-    # print(pos_list)
-    for pos, direction, num in pos_list:
-        if direction == 1:
-            pos = (pos + 1) % L
-            next_list.append([pos, direction, num])
+    cnt = 0
+    reverse_ant_pos = [item[0] for item in pos_list if item[1] == 2]
+    cnt += T//L * len(reverse_ant_pos)
 
-        else:
-            pos = (pos -1) % L
-            if pos < 0:
-                pos += 8
-            next_list.append([pos, direction, num])
-        
-        num += 1
+    for i in reverse_ant_pos:
+        if (i - first_ant[0])//2 +1 <= (T % L):
+            cnt += 1
+    
+    last_num_of_first_ant = (1 + cnt) % N
+    if last_num_of_first_ant == 0:
+        last_num_of_first_ant += N
 
-    for i in range(N):
-        
-        is_contact = False
+else:
+    last_pos_of_first_ant = (first_ant[0] - T ) % L
+    num_of_reverse_ant = [item[1] for item in pos_list].count(1)
 
-        if next_list[i][1] == 1:
-            if next_list[i][0] == (next_list[(i+1)%N][0] + 1) % L:
-                next_list[i][1], next_list[(i+1)%N][1] = next_list[(i+1)%N][1], next_list[i][1]
-                next_list[i][0], next_list[(i+1)%N][0] = next_list[(i+1)%N][0], next_list[i][0]
-        else:
-            if next_list[i][0] == (next_list[(i-1)%N][0] - 1) % L:
-                next_list[i][1], next_list[(i-1)%N][1] = next_list[(i-1)%N][1], next_list[i][1]
-                next_list[i][0], next_list[(i-1)%N][0] = next_list[(i-1)%N][0], next_list[i][0]
+    cnt = 0
+    reverse_ant_pos = [item[0] for item in pos_list if item[1] == 1]
 
-    T -= 1
+    cnt += T//L * len(reverse_ant_pos)
 
-# print(next_list)
+    for i in reverse_ant_pos:
+        if ((first_ant[0] -(i-L))//2 + 1) <= (T % L):
+            cnt += 1
+            # print(T % L)
+            # print(i, first_ant[0] -(i-L),(first_ant[0] -(i-L))//2 + 1) 
+            # print('cnt', cnt)
+    
+    last_num_of_first_ant = (1 - cnt) % N
+    if last_num_of_first_ant <= 0:
+        last_num_of_first_ant += N
 
-for item in next_list:
-    print(item[0])
+# アリの位置を求める
+last_ant_pos = []
+
+for ant_pos, direction  in pos_list:
+    if direction == 1:
+        last_ant_pos.append((ant_pos + T)%L)
+    else:
+        ant_pos = (ant_pos - T)%L
+        if ant_pos < 0:
+            ant_pos += L
+        last_ant_pos.append(ant_pos)
+
+# first ant さんは位置Xにいるので、アリの位置リストからどれがfirst antさんか特定する。
+# 特定したらその位置にゼッケンを設定する。
+# 1方向のfirst antさんは、その他のゼッケンは時計回りに+1する（他のアリを1減らしながら回っているから）
+# 2方向のfirst antさんは、その他のゼッケンを時計回りに-1する（他のアリを1増やしながら回っているから。逆回りに・・・で考えたほうが分かりよいか？）
+# 2方向の場合はできたリストを逆順に出力する    
+res = []
+if first_ant[1] == 1:
+    for i, pos in enumerate(last_ant_pos):
+            temp_num = (last_num_of_first_ant+i)%N
+            if temp_num == 0:
+                temp_num = N
+            res.append([pos, temp_num])
+
+    res.sort(key=lambda x:x[1])
+else:
+    for i, pos in enumerate(last_ant_pos):
+        temp_num = (last_num_of_first_ant-i)%N
+        if temp_num == 0:
+            temp_num = N
+        res.append([pos, temp_num])
+
+    res.sort(key=lambda x:x[1], reverse=True)
+
+
+
+# print(first_ant)
+# print(num_of_reverse_ant)
+# print(reverse_ant_pos)
+# print('collison', cnt)
+# print('first ant of pos, num',last_pos_of_first_ant, last_num_of_first_ant)
+# print(last_ant_pos)
+# print(res)
+# print('-------ans--------')
+print(*[item[0] for item in res])
