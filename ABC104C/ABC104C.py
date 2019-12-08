51b97adec8f959c8a503a19ce449acfa47ee5bc9
sho Taguchi <s.taguchi0703@gmail.com>
2019-12-09 01:56:16 +0900

    ABC104Cが難しい

--- ABC104C/ABC104C.py
+++ ABC104C/ABC104C.py
@@ -22,4 +22,32 @@ comp_bounas_list = [[int(item) for item in input().split()] for _ in range(D)]
 
 
 
-print(comp_bounas_list)
+comp_prob_list = []
+
+bit_list = []
+
+for i in range(2**D):
+    comp_prob = []
+    total_prob_n = 0
+    total_point = 0
+    bit_flag = ''
+    for j in range(D):
+        if ((i >> j) & 1 == 1):
+            total_prob_n += comp_bounas_list[j][0]
+            total_point += comp_bounas_list[j][0]*(j+1)*100 + comp_bounas_list[j][1]
+            bit_flag +='1'
+        else:
+            comp_prob.append([0, 0])
+            bit_flag +='0'
+
+    comp_prob_list.append([total_prob_n, total_point])
+    bit_list.append(bit_flag)
+
+print(comp_prob_list)
+print(bit_list)
+
+# どのbitが立ってるときターゲット以上になるか？
+# 一段少ないとこをとったとき、上より少ない数でターゲットをとれるか？
+
+
+
