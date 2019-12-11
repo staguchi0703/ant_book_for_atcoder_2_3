ae4b553c30779b7fdbdbcdb2162c06921f09445d
Sho Taguchi <taguchi.sho@mail.canon>
2019-12-11 13:15:16 +0900

    ABC002D検討中

--- ABC002D/ABC002D.py
+++ ABC002D/ABC002D.py
@@ -2,7 +2,7 @@
 # VScodeで入力をテキストから読み込んで標準入力に渡す
 import sys
 import os
-f=open(r'.\template\input.txt', 'r', encoding="utf-8")
+f=open(r'.\ABC002D\input.txt', 'r', encoding="utf-8")
 # inputをフルパスで指定
 # win10でファイルを作るとs-jisで保存されるため、読み込みをutf-8へエンコードする必要あり
 # VScodeでinput file開くとutf8になってるんだけど中身は結局s-jisになっているらしい
@@ -16,6 +16,13 @@ sys.stdin=f
 ##################################
 # %%
 # 以下ペースト可
-num_list = [int(item) for item in input().split()]
-print('F', num_list)
+N, M = [int(item) for item in input().split()]
+relation_list = [[int(item) for item in input().split()] for _ in range(M)]
 
+print(N,M)
+print(relation_list)
+
+# 
+for i in range(1, N+1):
+    for j in range(i+1, N+1):
+        print(i,'->',j)
