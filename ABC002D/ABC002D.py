cf8fcae8b158bd95afb0df400ffd762cb0bf84a9
Sho Taguchi <s.taguchi703@gmail.com>
2019-12-14 15:50:04 +0900

    数え上げる方法がわからない

--- ABC002D/ABC002D.py
+++ ABC002D/ABC002D.py
@@ -18,14 +18,15 @@ sys.stdin=f
 # 以下ペースト可
 N, M = [int(item) for item in input().split()]
 relation_list = [[int(item) for item in input().split()] for _ in range(M)]
+relation_list.sort(key= lambda x:x[0])
 
-print(N,M)
+# print(N,M)
 print(relation_list)
 
 combo_list = []
 
 for i in range(2**N):
-    bit_ex = print(format(i, 'b').zfill(N))
+    # print(format(i, 'b').zfill(N))
 
     temp_combo_list = []
     # 各人について関係がある人をリスト化していく
@@ -36,14 +37,30 @@ for i in range(2**N):
             if ((i >> j) & 1) == 1 and ((i >> k) & 1) == 1:
                 temp_combo.append([k+1, j+1])
         if temp_combo != []:
-            print(temp_combo[1:])
+            # print(temp_combo[1:])
             temp_combo_list += temp_combo[1:]
-    combo_list.append(temp_combo_list)
-print('ans check -------------------------------')
+    combo_list.append([temp_combo_list, i])
+# print('ans check -------------------------------')
 
-print(combo_list[7])
+# print(combo_list)
 
-if relation_list in temp_combo_list:
-    print(relation_list)
+res_list = []
+for item in combo_list:
+    # print(item[0])
+
+    is_found = 1
+    for relation in relation_list:
+        if relation in item[0]:
+            pass
+        else:
+            is_found = 0
+            break
+    
+    if is_found:
+        res_list.append(format(item[1],'b').count('1'))
+        print(bin(item[1]))
+
+if res_list == []:
+    print(1)
 else:
-    print('no way!')
+    print(max(res_list))
