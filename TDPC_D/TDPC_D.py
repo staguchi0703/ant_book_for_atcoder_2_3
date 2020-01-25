ec62562b06562f40bc654c394a3031cd026ad4c8
Sho Taguchi <s.taguchi703@gmail.com>
2020-01-26 00:33:35 +0900

    TDPCのDができた

--- TDPC_D/TDPC_D.py
+++ TDPC_D/TDPC_D.py
@@ -20,19 +20,42 @@ sys.stdin=f
 # 回答は　Dの倍数の確率なので計算して出力する
 import math
 N, D= [int(item) for item in input().split()]
-max_num = int(math.sqrt(6**N))
 
-dp = [[1 if m == D else 0 for m in range(max_num+1)] for _ in range(N+1)]
-cnt = 0
+factor = [0, 0, 0]
 
-for i in range(N):
-    for j in range(max_num+1):
-        for k in range(1,7):
-            dp[i+1][j] = dp[i][j]
-            if dp[i][j] == 1 and j*k < max_num:
-                index_1  = j*k
-                print(i,j,i*j, max_num)
-                dp[i+1][index_1] = 1
-                cnt += 1
+while D % 2 == 0:
+    D //= 2
+    factor[0] += 1
 
-print(cnt)
+while D % 3 == 0:
+    D //= 3
+    factor[1] += 1
+
+while D % 5 == 0:
+    D //= 5
+    factor[2] += 1
+
+if D != 1:
+    print(0)
+else:
+    dp = [[[[0.] * (factor[2]+1)
+            for j in range(factor[1]+1)]
+            for k in range(factor[0]+1)] 
+            for l in range(N+1)]
+    # 確率を値に持ったdp
+
+    dp[0][0][0][0] = 1 # 0回振って2,3,5の倍数になる確率は1（0だから）として初期化
+
+    for i in range(N):
+        for j in range(factor[0]+1):
+            for k in range(factor[1]+1):
+                for l in range(factor[2]+1):
+                    # i+1回目の確率
+                    # Dの倍数だから各素因数の数はDの因数の個数を超えてもよい。
+                    dp[i+1][j][k][l] += dp[i][j][k][l] * 1/6 #目が1
+                    dp[i+1][min(factor[0], j+1)][k][l] += dp[i][j][k][l] * 1/6#目が2
+                    dp[i+1][j][min(k+1,factor[1])][l] += dp[i][j][k][l] * 1/6#目が3
+                    dp[i+1][min(j+2,factor[0])][k][l] += dp[i][j][k][l] *  1/6#目が4
+                    dp[i+1][j][k][min(l+1,factor[2])] += dp[i][j][k][l] * 1/6#目が5
+                    dp[i+1][min(j+1,factor[0])][min(k+1,factor[1])][l] += dp[i][j][k][l] * 1/6#目が6
+    print(dp[N][factor[0]][factor[1]][factor[2]])
